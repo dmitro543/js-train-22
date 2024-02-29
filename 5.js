3158b6db2faaf06f0748883dcef3f0a96b6582e0
dmitro543 <123867495+dmitro543@users.noreply.github.com>
2024-02-29 14:11:13 +0200

    commit

--- 5.js
+++ 5.js
@@ -1,5 +1,7 @@
 // Мементо (Memento) — це патерн програмування, який забезпечує збереження стану об'єкта для подальшого відновлення
 
+// const { version } = require("react");
+
 // Клас Writer відповідає за роботу з текстом.
 class Writer {
   // Властивість #content представляє поточний текст. Вона ініціалізується порожнім рядком.
@@ -11,6 +13,20 @@ class Writer {
   // Він викликає статичний метод класу Version, create, передаючи йому поточний текст як аргумент.
   // Метод restore відновлює попередній стан тексту, викликаючи статичний метод класу Version, restore.
   // Цей метод повертає останню збережену версію тексту, яку ми встановлюємо як поточний текст.
+  #content = "";
+  set content(newContent) {
+    this.#content = newContent;
+    this.#store();
+  }
+  get content() {
+     return this.#content;
+  }
+  #store() {
+    Version.create(this.#content);
+  }
+  restore() {
+    this.#content = Version.restore().content;
+  }
 }
 
 // Клас Version відповідає за створення та зберігання версій тексту.
@@ -22,25 +38,37 @@ class Version {
   // Створений екземпляр додається до масиву версій versions.
   // Статичний метод restore видаляє останный элемент масиву,
   // та повертає останню збережену версію тексту з масиву версій this.#versions[this.#versions.length - 1] .
+  static #versions = [];
+  constructor (content) {
+     this.content = content;
+  }
+  static create(content) {
+    const text = new Version(content);
+    this.#versions.push(text);
+  }
+  static restore() {
+    this.#versions.pop();
+    return this.#versions[this.#versions.length - 1];
+  }
 }
 console.log("Завдання 5 ====================================");
 // Після виконання розкоментуйте код нижче
 
 // Створюємо новий екземпляр класу Writer
-// const writer = new Writer();
+const writer = new Writer();
 
 // Присвоюємо текст за допомогою сетера
-// writer.content = "Це початковий текст.";
-// writer.content = "Редагований текст.";
-// writer.content = "Оновлений текст.";
+writer.content = "Це початковий текст.";
+writer.content = "Редагований текст.";
+writer.content = "Оновлений текст.";
 
 // Друкуємо поточний текст
-// console.log(writer.content);
+console.log(writer.content);
 
-// Відновлюємо попередній текст
-// writer.restore();
-// console.log(writer.content);
+// новлюємо попередній текст
+writer.restore();
+console.log(writer.content);
 
 // Ще раз відновлюємо попередній текст
-// writer.restore();
-// console.log(writer.content);
+writer.restore();
+console.log(writer.content);
